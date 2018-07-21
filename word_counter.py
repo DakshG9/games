ef15e3641acb589ed51e6874b011222cccb15f0d
DakshG9 <41491524+DakshG9@users.noreply.github.com>
2018-07-21 16:17:26 -0400

    Findi out somethings!
    tells the amount of characters, words, and sentences in a file(in the same folder)

--- word_counter.py
+++ word_counter.py
@@ -1,15 +1,52 @@
+active = True
+print
 
-try:
-    file_name = text.txt
-except NameError:
-    msg = "Make sure sure you typed 'text.txt' like this!"
- 
-    print(msg)
+while active == True:
+    print('Starting up...')
 
-try:
-    with open(file_name, encoding='utf-8') as f_oj:
-        contents = f_obj.read()
+    try:
+        file_name = input('Which file do you want to scan?')
+        print("type 'q' to quit. ")
+        if file_name == 'q':
+            active = False
+            break
+            print('Cleaning up...\nExiting program...\nThank you!')
+        print('Scanning...')
+    except NameError:
+        msg = "The file finder has a bug. We can't find your file"
+     
+        print(msg)
 
-except FileNotFoundError:
-    msg = "We can't find " + file_name
-    print(msg)
+    try:
+        with open(file_name, encoding='utf-8') as f_obj:
+            contents = f_obj.read()
+
+    except FileNotFoundError:
+        msg = "We can't find " + file_name
+        print(msg)
+
+    else:
+        num_words = str(len(contents.split())) + " words"
+        num_characters = str(len(contents)) + " characters"
+        period = len(contents.split('.'))
+        question = len(contents.split('?'))
+        exclamation = len(contents.split('!'))
+        num_sentences =  str(period + question + exclamation)+ ' sentences'
+     
+                                                                                                
+        print('After scanning ' + file_name + ', we have gathered the following results:')
+        print('The file ' + file_name + " has:")
+        print(num_words)
+        print(num_characters)
+        print(num_sentences)
+
+        try:
+            repeat = input('Do you want to scan another file(yes/no)?')
+            if repeat == 'no':
+                active = False
+                print('Cleaning up...\nThank you!')
+            else:
+                print('Cleaning up...\nReloading...')
+        except NameError:
+            print('Sorry, we couldn\'t load up the full program')
+
